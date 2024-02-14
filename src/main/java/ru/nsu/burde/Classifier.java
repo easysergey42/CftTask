e0aed762da05a915a605c5664d225ecf0fe95f1b
s.burde <s.burde@g.nsu.ru>
2024-02-15 03:07:21 +0700

    Seems stable but no statistics

--- src/main/java/ru/nsu/burde/Classifier.java
+++ src/main/java/ru/nsu/burde/Classifier.java
@@ -2,6 +2,8 @@ package ru.nsu.burde;
 
 import java.io.BufferedWriter;
 import java.io.Writer;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,11 +16,31 @@ public class Classifier {
     }
 
 
+
 //    public ClassifiedType determineType(String s){
 //
 //    }
+    public static ClassifiedType classifyString(String s){
+        if (isInteger(s)) return ClassifiedType.INTEGER;
+        if (isFloat(s)) return ClassifiedType.FLOAT;
+        return ClassifiedType.STRING;
+    }
 
     public static boolean isInteger(String in){
-        return in.matches("-?\\d");
+        try {
+            var x = new BigInteger(in);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static boolean isFloat(String in){
+        try {
+            var x = new BigDecimal(in);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
--- src/main/java/ru/nsu/burde/Utils.java
+++ src/main/java/ru/nsu/burde/Utils.java
@@ -3,60 +3,95 @@ package ru.nsu.burde;
 import org.apache.commons.cli.*;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class Utils {
 
-    public enum DataTypes{
-        INTEGER,
-        FLOAT,
-        STRING
+
+    private static List<BufferedReader> readers;
+    private static Map<Classifier.ClassifiedType, BufferedWriter> writers = new HashMap<>();
+
+    private static void setWriters(){
+        writers = new HashMap<>();
+        for (Classifier.ClassifiedType key : Classifier.ClassifiedType.values()) {
+            writers.put(key, null);
+        }
     }
 
+    private static void flushWriters() throws IOException {
+        for (BufferedWriter value : writers.values()) {
+            if (value != null){
+                value.flush();
+            }
+        }
+    }
 
+    private static void closeWriters() throws IOException {
+        for (BufferedWriter value : writers.values()) {
+            if (value != null){
+                value.close();
+            }
+        }
+    }
 
-    public static void processInput(){
-
-        List<BufferedReader> readers = new ArrayList<>();
-
+    private static void setReaders(String[] inputFileNames){
+        readers = new ArrayList<>();
         for (var file :
-                Globals.inputFileNames) {
+                inputFileNames) {
             try {
-                BufferedReader reader = new BufferedReader(new FileReader(file));
+                BufferedReader reader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8));
                 readers.add(reader);
-            } catch (FileNotFoundException e) {
-//                throw new RuntimeException(e);
+            } catch (IOException e) {
                 System.err.println(e.getMessage());
             }
         }
+    }
 
-        try(BufferedWriter StringWriter = new BufferedWriter(new FileWriter("govno.txt"));
-            BufferedWriter IntegerWriter = new BufferedWriter(new FileWriter("govno.txt"));
-            BufferedWriter FractionalWriter = new BufferedWriter(new FileWriter("govno.txt")))
-        {
-            while(readers.size()>0){
-                for (var r:
-                        readers) {
+    private static void writeToFile(Classifier.ClassifiedType type, String value) throws IOException {
+        var writer = writers.getOrDefault(type, null);
+        if (writer == null){
+            File dir = new File(Globals.outputPath);
+            if (!dir.exists()) dir.mkdirs();
+            File f = new File(dir, Globals.filePrefix + Globals.typeToFileName.get(type));
 
-                    String line = r.readLine();
+            writer = new BufferedWriter(new FileWriter(f, StandardCharsets.UTF_8));
+            writers.put(type, writer);
+        }
+        writer.write(value);
+        writer.newLine();
+    }
+
+    public static void processInput() {
+        setReaders(Globals.inputFileNames);
+
+
+        try {
+            while (!readers.isEmpty()) {
+                var iter = readers.listIterator();
+                while (iter.hasNext()){
+                    var reader = iter.next();
+                    String line = reader.readLine();
                     if (line == null){
-                        readers.remove(r);
-                        r.close();
+                        reader.close();
+                        iter.remove();
                     }
                     else{
-                    }
+                        var t = Classifier.classifyString(line);
 
+                        writeToFile(t, line);
+                    }
                 }
             }
-
+            flushWriters();
+            closeWriters();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-
-
-//        System.out.println(readers);
     }
 
     public static void parseCommandLineArgs(String[] args){
